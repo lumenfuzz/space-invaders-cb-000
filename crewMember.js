d81f8ee155bbac7ed9c347e8f6790122dbf6f88a
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-13 17:01:11 +0000

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,28 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = "Looking for a Rig";
+    this.engageWarpDrive = () => {return "had no effect"}
+    this.setsInvisibility = () => {
+      if (this.currentShip != "Looking for a Rig" && this.position == "Defender") {
+        this.currentShip.cloaked = true
+      }
+
+      return "had no effect"
+    }
+    this.chargePhasers = () => {return "had no effect"}
+    switch(position) {
+      case "Pilot":
+
+        break;
+      case "Defender":
+
+        break;
+      case "Gunner":
+
+        break;
+      default:
+        null
+    }
+  }
+}
--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,13 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.phasersCharge = "uncharged";
+    crew.length === 0 ? this.docked = true : this.docked = false
+    crew.forEach( (e) => { e.currentShip = this })
+    this.crew = crew
+  }
+}
