8a9b37495e90f31a73716f98909364926e173102
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-13 13:15:03 +0000

    automatically backed up by learn

--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,10 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, array, phasers, shields) {
+    this.name = name;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.phasersCharge = "uncharged"
+  }
+}
