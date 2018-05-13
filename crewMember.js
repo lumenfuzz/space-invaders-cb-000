efa5246d992510c3d122142964250cec560eeede
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-13 16:50:38 +0000

    automatically backed up by learn

--- crewMember.js
+++ crewMember.js
@@ -2,5 +2,18 @@ class CrewMember {
   constructor(position) {
     this.position = position;
     this.currentShip = "Looking for a Rig";
+    switch(position) {
+      case "Pilot":
+        this.engageWarpDrive = "had no effect"
+        break;
+      case "Defender":
+        this.setsInvisibility = "had no effect"
+        break;
+      case "Gunner":
+        this.chargePhasers = "had no effect"
+        break;
+      default:
+        null  
+    }
   }
 }
